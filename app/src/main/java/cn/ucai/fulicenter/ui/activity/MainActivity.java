bc87903870bd8572578f6ed58b5dd8e103ad76b2
BoomBone <ticccccc@163.com>
2017-05-04 11:20:22 +0800

    更改测试里的网络请求

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/MainActivity.java
@@ -7,8 +7,8 @@ import android.view.View;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.data.bean.NewGoodsBean;
 import cn.ucai.fulicenter.data.net.GoodsModel;
+import cn.ucai.fulicenter.data.net.OnCompleteListener;
 import cn.ucai.fulicenter.data.utils.L;
-import cn.ucai.fulicenter.data.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void testDownloadNewGoods(){
         GoodsModel model = new GoodsModel();
-        model.loadNewGoodsData(MainActivity.this, 0, 1, 10, new OkHttpUtils.OnCompleteListener<NewGoodsBean[]>() {
+        model.loadNewGoodsData(MainActivity.this, 0, 1, 10, new OnCompleteListener<NewGoodsBean[]>() {
             @Override
             public void onSuccess(NewGoodsBean[] result) {
                 L.e("main","result"+result);
