931455a3cce72bfc343ead9d81f4ff6b79d647e7
BoomBone <ticccccc@163.com>
2017-05-05 14:11:27 +0800

    精选首页数据下载

---
+++ app/src/main/java/cn/ucai/fulicenter/ui/fragment/BotiqueFragment.java
@@ -0,0 +1,81 @@
+package cn.ucai.fulicenter.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.data.bean.BoutiqueBean;
+import cn.ucai.fulicenter.data.net.GoodsModel;
+import cn.ucai.fulicenter.data.net.IGoodsModel;
+import cn.ucai.fulicenter.data.net.OnCompleteListener;
+import cn.ucai.fulicenter.data.utils.L;
+import cn.ucai.fulicenter.data.utils.ResultUtils;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+
+public class BotiqueFragment extends Fragment {
+    private static final String TAG = "BoutiqueFragment";
+    @BindView(R.id.tv_refresh)
+    TextView tvRefresh;
+    @BindView(R.id.rv_goods)
+    RecyclerView rvGoods;
+    @BindView(R.id.srf)
+    SwipeRefreshLayout srf;
+    @BindView(R.id.tv_nomore)
+    TextView tvNomore;
+    Unbinder unbinder;
+
+    IGoodsModel model;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_newgoods, null);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        model = new GoodsModel();
+        loadData();
+    }
+
+    private void loadData() {
+        model.loadBoutiqueData(getContext(), new OnCompleteListener<BoutiqueBean[]>() {
+            @Override
+            public void onSuccess(BoutiqueBean[] result) {
+                L.e(TAG,"resule="+result);
+                if (result != null) {
+                    ArrayList<BoutiqueBean> list = ResultUtils.array2List(result);
+                }
+            }
+
+            @Override
+            public void onError(String error) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+}
