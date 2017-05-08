1baa453b168618e957b436dc6c64a429c0c9ff64
BoomBone <ticccccc@163.com>
2017-05-08 15:28:24 +0800

    商品分类，大类、小类下载的接口及其实现

--- app/src/main/java/cn/ucai/fulicenter/data/net/GoodsModel.java
+++ app/src/main/java/cn/ucai/fulicenter/data/net/GoodsModel.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import cn.ucai.fulicenter.application.I;
 import cn.ucai.fulicenter.data.bean.BoutiqueBean;
+import cn.ucai.fulicenter.data.bean.CategoryChildBean;
+import cn.ucai.fulicenter.data.bean.CategoryGroupBean;
+import cn.ucai.fulicenter.data.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.data.bean.NewGoodsBean;
 import cn.ucai.fulicenter.data.utils.OkHttpUtils;
 
@@ -16,18 +19,45 @@ public class GoodsModel implements IGoodsModel {
     public void loadNewGoodsData(Context context, int catId, int pageId, int pageSize, OnCompleteListener<NewGoodsBean[]> listener) {
         OkHttpUtils<NewGoodsBean[]> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
-                .addParam(I.NewAndBoutiqueGoods.CAT_ID,String.valueOf(catId))
-                .addParam(I.PAGE_ID,String.valueOf(pageId))
-                .addParam(I.PAGE_SIZE,String.valueOf(pageSize))
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID, String.valueOf(catId))
+                .addParam(I.PAGE_ID, String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE, String.valueOf(pageSize))
                 .targetClass(NewGoodsBean[].class)
                 .execute(listener);
     }
 
     @Override
-    public void loadBoutiqueData(Context context, OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener) {
+    public void loadBoutiqueData(Context context, OnCompleteListener<BoutiqueBean[]> listener) {
         OkHttpUtils<BoutiqueBean[]> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_BOUTIQUES)
                 .targetClass(BoutiqueBean[].class)
+                .execute(listener);
+    }
+
+    @Override
+    public void loadGoodsDetailData(Context context, int goodId, OnCompleteListener<GoodsDetailsBean> listener) {
+        OkHttpUtils<GoodsDetailsBean> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
+                .addParam(I.GoodsDetails.KEY_GOODS_ID, String.valueOf(goodId))
+                .targetClass(GoodsDetailsBean.class)
+                .execute(listener);
+
+    }
+
+    @Override
+    public void loadCreategoryGroup(Context context, OnCompleteListener<CategoryGroupBean[]> listener) {
+        OkHttpUtils<CategoryGroupBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_GROUP)
+                .targetClass(CategoryGroupBean[].class)
+                .execute(listener);
+    }
+
+    @Override
+    public void loadCreategoryChild(Context context, int parentId, OnCompleteListener<CategoryChildBean[]> listener) {
+        OkHttpUtils<CategoryChildBean[]> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_FIND_CATEGORY_CHILDREN)
+                .addParam(I.CategoryChild.PARENT_ID, String.valueOf(parentId))
+                .targetClass(CategoryChildBean[].class)
                 .execute(listener);
     }
 }
--- app/src/main/java/cn/ucai/fulicenter/data/net/IGoodsModel.java
+++ app/src/main/java/cn/ucai/fulicenter/data/net/IGoodsModel.java
@@ -3,6 +3,9 @@ package cn.ucai.fulicenter.data.net;
 import android.content.Context;
 
 import cn.ucai.fulicenter.data.bean.BoutiqueBean;
+import cn.ucai.fulicenter.data.bean.CategoryChildBean;
+import cn.ucai.fulicenter.data.bean.CategoryGroupBean;
+import cn.ucai.fulicenter.data.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.data.bean.NewGoodsBean;
 import cn.ucai.fulicenter.data.utils.OkHttpUtils;
 
@@ -14,5 +17,12 @@ public interface IGoodsModel {
     void loadNewGoodsData(Context context, int catId , int pageId , int pageSize ,
                           OnCompleteListener<NewGoodsBean[]> listener);
 
-    void loadBoutiqueData(Context context, OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener);
+    void loadBoutiqueData(Context context, OnCompleteListener<BoutiqueBean[]> listener);
+
+    void loadGoodsDetailData(Context context, int goodId, OnCompleteListener<GoodsDetailsBean> listener);
+
+    void loadCreategoryGroup(Context context, OnCompleteListener<CategoryGroupBean[]> listener);
+
+    void loadCreategoryChild(Context context, int parentId, OnCompleteListener<CategoryChildBean[]> listener);
+
 }
