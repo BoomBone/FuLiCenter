56df7097ecceabc8978fa509c3b6f5d7e2b9644a
BoomBone <ticccccc@163.com>
2017-05-12 11:54:02 +0800

    实现头像的裁剪

--- app/src/main/java/cn/ucai/fulicenter/ui/activity/SettingActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/ui/activity/SettingActivity.java
@@ -1,11 +1,32 @@
 package cn.ucai.fulicenter.ui.activity;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,9 +40,30 @@ import cn.ucai.fulicenter.data.net.UserModel;
 import cn.ucai.fulicenter.data.utils.ImageLoader;
 import cn.ucai.fulicenter.data.utils.L;
 import cn.ucai.fulicenter.data.utils.SharePrefrenceUtils;
+import cn.ucai.fulicenter.ui.view.CircleImageView;
 
 
 public class SettingActivity extends AppCompatActivity {
+    //////////////////////////////////////////////////////////
+    //请求相机
+    private static final int REQUEST_CAPTURE = 100;
+    //请求相册
+    private static final int REQUEST_PICK = 101;
+    //请求截图
+    private static final int REQUEST_CROP_PHOTO = 102;
+    //请求访问外部存储
+    private static final int READ_EXTERNAL_STORAGE_REQUEST_CODE = 103;
+    //请求写入外部存储
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 104;
+    //头像1
+    private CircleImageView headImage1;
+    //头像2
+    private ImageView headImage2;
+    //调用照相机返回图片临时文件
+    private File tempFile;
+    // 1: qq, 2: weixin
+    private int type;
+    /////////////////////////////////////////////////////////
     String TAG = "SettingActivity";
     @BindView(R.id.tv_common_title)
     TextView mTvCommonTitle;
@@ -41,6 +83,8 @@ public class SettingActivity extends AppCompatActivity {
         mTvCommonTitle.setText(R.string.setting);
         initData();
         model = new UserModel();
+
+        createCameraTempFile(savedInstanceState);
     }
 
     private void initData() {
@@ -51,7 +95,7 @@ public class SettingActivity extends AppCompatActivity {
             ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user), SettingActivity.this,
                     mIvUserAvatar);
         }
-        L.e(TAG,"initData().user.getMuserName()"+user.getMuserName()+"user.getMuserNick()"+user.getMuserNick());
+        L.e(TAG, "initData().user.getMuserName()" + user.getMuserName() + "user.getMuserNick()" + user.getMuserNick());
     }
 
     @OnClick({R.id.backClickArea, R.id.btn_logout})
@@ -76,14 +120,254 @@ public class SettingActivity extends AppCompatActivity {
 
     @OnClick(R.id.rl_nickname)
     public void updateNick() {
-        startActivityForResult(new Intent(SettingActivity.this,UpdateNickActivity.class),I.REQUEST_CODE_NICK);
+        startActivityForResult(new Intent(SettingActivity.this, UpdateNickActivity.class), I.REQUEST_CODE_NICK);
+    }
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_NICK) {
+            initData();
+        }
+    }*/
+
+    @OnClick(R.id.iv_user_avatar)
+    public void onPhoto() {
+        uploadHeadImage();
+    }
+    //--upload avatar start
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void uploadHeadImage() {
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_popupwindow, null);
+        TextView btnCarema = (TextView) view.findViewById(R.id.btn_camera);
+        TextView btnPhoto = (TextView) view.findViewById(R.id.btn_photo);
+        TextView btnCancel = (TextView) view.findViewById(R.id.btn_cancel);
+        final PopupWindow popupWindow = new PopupWindow(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setBackgroundDrawable(getResources().getDrawable(android.R.color.transparent));
+        popupWindow.setOutsideTouchable(true);
+        View parent = LayoutInflater.from(this).inflate(R.layout.activity_setting, null);
+        popupWindow.showAtLocation(parent, Gravity.BOTTOM, 0, 0);
+        //popupWindow在弹窗的时候背景半透明
+        final WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.alpha = 1f;
+        getWindow().setAttributes(params);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                params.alpha = 1.0f;
+                getWindow().setAttributes(params);
+            }
+        });
+
+        btnCarema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //权限判断
+                if (ContextCompat.checkSelfPermission(SettingActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //申请WRITE_EXTERNAL_STORAGE权限
+                    ActivityCompat.requestPermissions(SettingActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+                } else {
+                    //跳转到调用系统相机
+                    gotoCarema();
+                }
+                popupWindow.dismiss();
+            }
+        });
+        btnPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //权限判断
+                if (ContextCompat.checkSelfPermission(SettingActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //申请READ_EXTERNAL_STORAGE权限
+                    ActivityCompat.requestPermissions(SettingActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            READ_EXTERNAL_STORAGE_REQUEST_CODE);
+                } else {
+                    //跳转到调用系统图库
+                    gotoPhoto();
+                }
+                popupWindow.dismiss();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+    }
+
+    /**
+     * 跳转到相册
+     */
+    private void gotoPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(Intent.createChooser(intent, "请选择图片"), REQUEST_PICK);
+    }
+
+
+    /**
+     * 跳转到照相机
+     */
+    private void gotoCarema() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+        startActivityForResult(intent, REQUEST_CAPTURE);
+    }
+
+    /**
+     * 创建调用系统照相机待存储的临时文件
+     *
+     * @param savedInstanceState
+     */
+    private void createCameraTempFile(Bundle savedInstanceState) {
+        if (savedInstanceState != null && savedInstanceState.containsKey("tempFile")) {
+            tempFile = (File) savedInstanceState.getSerializable("tempFile");
+        } else {
+            tempFile = new File(checkDirPath(Environment.getExternalStorageDirectory().getPath() + "/image/"),
+                    System.currentTimeMillis() + ".jpg");
+        }
+    }
+
+    /**
+     * 检查文件是否存在
+     */
+    private static String checkDirPath(String dirPath) {
+        if (TextUtils.isEmpty(dirPath)) {
+            return "";
+        }
+        File dir = new File(dirPath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return dirPath;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK&&requestCode== I.REQUEST_CODE_NICK){
-            initData();
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("tempFile", tempFile);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch (requestCode) {
+            case I.REQUEST_CODE_NICK:
+                if(resultCode == RESULT_OK){
+                    initData();
+                }
+                break;
+            case REQUEST_CAPTURE: //调用系统相机返回
+                if (resultCode == RESULT_OK) {
+                    gotoClipActivity(Uri.fromFile(tempFile));
+                }
+                break;
+            case REQUEST_PICK:  //调用系统相册返回
+                if (resultCode == RESULT_OK) {
+                    Uri uri = intent.getData();
+                    gotoClipActivity(uri);
+                }
+                break;
+            case REQUEST_CROP_PHOTO:  //剪切图片返回
+                if (resultCode == RESULT_OK) {
+                    final Uri uri = intent.getData();
+                    if (uri == null) {
+                        return;
+                    }
+                    String cropImagePath = getRealFilePathFromUri(getApplicationContext(), uri);
+                    Bitmap bitMap = BitmapFactory.decodeFile(cropImagePath);
+                    mIvUserAvatar.setImageBitmap(bitMap);
+
+                    /*if (type == 1) {
+                        headImage1.setImageBitmap(bitMap);
+                    } else {
+                        headImage2.setImageBitmap(bitMap);
+                    }*/
+                    //此处后面可以将bitMap转为二进制上传后台网络
+                    //......
+
+                }
+                break;
         }
     }
+
+    /**
+     * 打开截图界面
+     *
+     * @param uri
+     */
+    public void gotoClipActivity(Uri uri) {
+        Log.i("main", "uri:" + uri);
+        if (uri == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.setClass(this, ClipImageActivity.class);
+        intent.putExtra("type", type);
+        intent.setData(uri);
+        startActivityForResult(intent, REQUEST_CROP_PHOTO);
+    }
+
+
+    /**
+     * 根据Uri返回文件绝对路径
+     * 兼容了file:///开头的 和 content://开头的情况
+     *
+     * @param context
+     * @param uri
+     * @return the file path or null
+     */
+    public static String getRealFilePathFromUri(final Context context, final Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null)
+            data = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+    /**
+     * 外部存储权限申请返回
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                gotoCarema();
+            } else {
+                // Permission Denied
+            }
+        } else if (requestCode == READ_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                gotoPhoto();
+            } else {
+                // Permission Denied
+            }
+        }
+    }
+//--upload avatar end
 }
